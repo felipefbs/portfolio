1543ecd2eac72e95d532f7267dd33ba32c74da50
felipefbs <me@felipefbs.dev>
2024-02-03 00:32:49 -0300

    tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{templ,html}"],
+  theme: {
+    fontFamily: {
+      'body': ["Anonymous Pro", 'monospace']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
