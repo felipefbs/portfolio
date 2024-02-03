4dbc6696a91290a67acd64f0b41f200745cc0ddb
felipefbs <me@felipefbs.dev>
2024-02-03 10:13:33 -0300

    about page

---
+++ experience/education.go
@@ -0,0 +1,11 @@
+package experience
+
+type EducationItemProps struct {
+	Type             string
+	Title            string
+	Course           string
+	Organization     string
+	OrganizationLink string
+	Location         string
+	Duration         string
+}
