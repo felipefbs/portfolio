fdc6d76f9d040cd509023145a3f40b4cdd98a2e9
felipefbs <me@felipefbs.dev>
2024-02-03 00:33:10 -0300

    experience data

---
+++ experience/xp.go
@@ -0,0 +1,12 @@
+package experience
+
+type ExperienceItemProps struct {
+	JobTitle         string
+	Organization     string
+	OrganizationLink string
+	Location         string
+	Duration         string
+	Technologies     []string
+	Responsibilities []string
+	Languages        []string
+}
